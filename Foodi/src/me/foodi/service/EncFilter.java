fed525f8a0010367ecf23d971cbc819f36bf04e3
tzedeq <whiteus@gmail.com>
2019-06-13 17:57:05 +0900

    add EncFilter

---
+++ Foodi/src/me/foodi/service/EncFilter.java
@@ -0,0 +1,34 @@
+package me.foodi.service;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+
+@WebFilter("/ex")
+public class EncFilter implements Filter {
+
+
+    public EncFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		request.setCharacterEncoding("utf-8");
+		chain.doFilter(request, response);
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
